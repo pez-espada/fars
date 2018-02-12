ad21c09a8d42758a4f28422d412bbdfd2b4d6d15
Carlos J. Dommar <carlos.dommar@gmail.com>
2018-02-12 23:38:39 +0100

    adding the tests files and some mods on the R code

--- R/fars.R
+++ R/fars.R
@@ -150,3 +150,7 @@ fars_map_state <- function(state.num, year) {
                 graphics::points(LONGITUD, LATITUDE, pch = 46)
         })
 }
+
+# My tests
+#testthat::test_file(path = "inst/tests/my_tests.R")
+#testthat::test_dir(path = "inst/tests/", reporter = "minimal")
